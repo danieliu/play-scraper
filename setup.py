4e0a6e0fe5bda0ed511e76e63b6dd4bc8355d134
Daniel Liu <idaniel.liu@gmail.com>
2018-08-12 20:58:59 -0700

    Fix lxml version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ],
     install_requires=[
         'beautifulsoup4==4.4.1',
-        'lxml==3.6.0',
+        'lxml==4.2.4',
         'requests-futures==0.9.7',
         'requests[security]==2.9.1',
     ],
