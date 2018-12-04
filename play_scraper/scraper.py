413c0b595fb3889b93b35d2bfc1b8e8892b15e3b
Dr. Masroor Ehsan <masroore@gmail.com>
2018-12-04 11:58:13 +0600

    FIX #19 - Updated class attribute in PlayScraper.categories()

--- play_scraper/scraper.py
+++ play_scraper/scraper.py
@@ -253,14 +253,14 @@ class PlayScraper(object):
         Note: May contain some promotions, e.g. "Popular Characters"
         """
         categories = {}
-        strainer = SoupStrainer('a', {'class': 'child-submenu-link'})
+        strainer = SoupStrainer('a', {'class': 'r2Osbf'})
 
         response = send_request('GET', s.BASE_URL, params=self.params)
         soup = BeautifulSoup(response.content,
                              'lxml',
                              from_encoding='utf8',
                              parse_only=strainer)
-        category_links = soup.select('a.child-submenu-link')
+        category_links = soup.select('a.r2Osbf')
         age = '?age='
 
         for cat in category_links:
